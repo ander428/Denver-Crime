5751d28900a7b9a575c74ebf4f6c61769f90b035
ander428 <ander428@mail.chapman.edu>
2019-01-17 16:15:02 -0800

    Denver roadmaps

--- DenverMap.py
+++ DenverMap.py
@@ -1,14 +1,16 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 
-m = Basemap(projection='lcc', resolution='h',
-                lat_0=39.3, lon_0=-104.991,
-                width=0.9E6, height=0.6E6)
 
-m.drawcoastlines()
-m.drawcountries(linewidth=2)
-m.drawstates(color='b')
-m.drawcounties(color='darkred')
+class DenverMap(Basemap):
+    def __init__(self):
+        self.map = Basemap(projection='lcc', resolution='h',
+                    lat_0=39.3, lon_0=-104.991,
+                    width=0.9E6, height=0.6E6)
 
-plt.title('Basemap of Colorado')
-plt.show()
+    def show(self):
+        self.map.readshapefile('./Data/roads', 'Streets',drawbounds = True, color='grey')
+        plt.show()
+
+dm = DenverMap()
+dm.show()
