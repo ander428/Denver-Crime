0c89e2f582aa908fe4feb3c74bd78e8b72044c9b
ander428 <ander428@mail.chapman.edu>
2019-01-20 21:40:46 -0800

    Added Comments

--- DenverCrime.py
+++ DenverCrime.py
@@ -1,18 +1,65 @@
+# Matt Greenburg - 2255210
+# Joshua Anderson - 2270306
+# Eric Vela - 2277759
+#
+# This program takes in a csv file containing crime data in Denver
+# and returns analytics from DBSCAN, Markov, and HAC Algorithms
+#
+# DBSCAN - Density of Crimes based on Location or Neighborhood
+# Markov - Predict a Crime to occur in a certain Location
+# HAC - Associate crime frequency with Neighborhood
+
+import os
 import pandas as pd
 import zipfile
 from FOM import FOM
 
-zip_ref = zipfile.ZipFile("denver-crime-data.zip", 'r')
-zip_ref.extractall("./Data/CrimeCSV")
-zip_ref.close()
+''' DATA AQUISITION '''
 
-df = pd.read_csv("./Data/CrimeCSV/crime.csv", delimiter=',', encoding="utf-8-sig")
+# Unzip CSV file and create directory if not done already
+csv_path = "./Data/CrimeCSV/"
+csv_name = "crime.csv"
+if not os.path.isdir(csv_path) and not os.path.exists(csv_path + csv_name):
+    print("Extracting data...")
+    zip_ref = zipfile.ZipFile("denver-crime-data.zip", 'r')
+    zip_ref.extractall(csv_path)
+    zip_ref.close()
+    print("Done.")
 
-df2 = df[["REPORTED_DATE", "NEIGHBORHOOD_ID" , "OFFENSE_CATEGORY_ID"]].copy()
-df2.loc[:,"REPORTED_DATE"] = pd.to_datetime(df2.loc[:,"REPORTED_DATE"])
-df2.set_index('REPORTED_DATE', drop=True, append=False, inplace=True, verify_integrity=False)
-df2 = df2.sort_index()
+# Read CSV into pandas DataFrame
+print("Reading CSV file...")
+df = pd.read_csv((csv_path + csv_name), delimiter=',', encoding="utf-8-sig")
+print("Done.")
 
+''' PREDICT OFFENSE CATEGORY BY NEIGHBORHOOD '''
+
+# Create subset of crimes containing only REPORTED_DATE, NEIGHBORHOOD_ID, and OFFENSE_CATEGORY_ID
+print("Loading Data for Markov...")
+crimeByNID = df[["REPORTED_DATE", "NEIGHBORHOOD_ID" , "OFFENSE_CATEGORY_ID"]].copy()
+crimeByNID.loc[:,"REPORTED_DATE"] = pd.to_datetime(crimeByNID.loc[:,"REPORTED_DATE"])
+crimeByNID.set_index('REPORTED_DATE', drop=True, append=False, inplace=True, verify_integrity=False)
+crimeByNID = crimeByNID.sort_index()
+print("Done.")
+
+# Initialize and train First Order Markov Model
+print("Predicting Offense Category by Neighborhood...")
 markov = FOM()
-markov.learn(df2, ["NEIGHBORHOOD_ID", "OFFENSE_CATEGORY_ID"])
-pred = markov.choose() # returns dict of predictions for each neighborhood
+
+# Train Crime Data
+#   Parameters:
+#       DataFrame - Contains REPORTED_DATE, NEIGHBORHOOD_ID, and OFFENSE_CATEGORY_ID
+#       List - Consists of 2 column names for keys and values respectively
+markov.learn(crimeByNID, ["NEIGHBORHOOD_ID", "OFFENSE_CATEGORY_ID"])
+
+# Markov Crime Predicion
+#   Parameters - Neighborhood, Crime
+#   Output - Returns next predicted crime in the given neighborhood
+prediction = markov.predict("cbd", "burglary") # example
+print("Done.")
+
+''' PREDICT NEIGHBORHOOD BY OFFENSE CATEGORY '''
+
+# Changes to predicting neighborhood by offense category
+# by switching the keys an values
+markov.learn(crimeByNID, ["OFFENSE_CATEGORY_ID", "NEIGHBORHOOD_ID"])
+prediction = markov.predict("burglary", "cbd") # example
--- FOM.py
+++ FOM.py
@@ -1,30 +1,76 @@
+# Matt Greenburg - 2255210
+# Joshua Anderson - 2270306
+# Eric Vela - 2277759
+#
+# This class takes in a pandas DataFrame and returns a prediction
+#  of a value based on a First Order Markov Model
+
 import numpy as np
 import random as rand
+import pandas as pd
 
 class FOM:
 
+    # Constructor
     def __init__(self):
-        self.memory = {}
-        self.predictions = []
+        self.MAX_ROWS = 10000   # Set a limit on how many rows are trained
+        self.memory = {}        # Dictionary to store data
 
+    # Train train on pandas DataFrame given key and value columns
     def learn(self, dataFrame, columns):
-        self.memory = dict(zip(dataFrame[columns[0]], dataFrame[columns[1]]))
+        keyCol = columns[0] # column to be used as keys
+        valCol = columns[1] # column to be used as values
 
-    def choose(self):
-        randVal = rand.random()
-        total = 0
-        for key, value in self.memory.items():
-            uniqueVals = value
-            print("Key:", key, "Value:", uniqueVals)
+        # iterate through every row in DataFrame
+        for i, row in enumerate(dataFrame.iterrows()):
+            row = row[1] # get row out of tuple from iterrows()
 
-    def nextWord(self, key):
-        values = self.memory[key]
+            # Stop training if max rows is reached
+            if(i >= self.MAX_ROWS):
+                break
+
+            # Add to dictionary
+            if row[keyCol] not in self.memory.keys():
+                self.memory[row[keyCol]] = []
+            self.memory[row[keyCol]].append(row[valCol])
+
+    # Predicts next value given the current value
+    def predict(self, category, key):
+        data = self.listToDict(self.memory[category]) # get dictionary of all values
+        values = data[key]
         count = {}
-        for i in values:
-            if i not in count.keys():
-                count[i] = 0
-            count[i] += 1
+
+        # Iterate through each value for the given key
+        # and calulate probabilities
+        for val in values:
+            if val not in count.keys():
+                count[val] = 0
+            count[val] += 1
         for key in count.keys():
             count[key] = count[key] / len(values)
-        nw = choose(count)
-        return nw
+
+        # make prediction based on probabilities
+        pred = self.choose(count)
+        return pred
+
+    # Chooses a value in a given dictionary of probabilities
+    def choose(self, dict):
+        randVal = rand.random()
+        total = 0
+        for key, value in dict.items():
+            total += value
+            if randVal <= total:
+                return key
+
+    # Converts a list to a dictionary containing keys of each
+    # unique value and lists of "next values"
+    def listToDict(self, list):
+        dict = {}
+        for i, value in enumerate(list):
+            if value not in dict.keys():
+                dict[value] = []
+            try:
+                dict[value].append(list[i+1])
+            except:
+                pass
+        return dict
