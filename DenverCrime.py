1bafe12bc3c3b3192e0c009cbd173d4b630edaad
ander428 <ander428@mail.chapman.edu>
2019-01-20 18:57:29 -0800

    Markov template

--- DenverCrime.py
+++ DenverCrime.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import zipfile
+from FOM import FOM
 
 zip_ref = zipfile.ZipFile("denver-crime-data.zip", 'r')
 zip_ref.extractall("./Data/CrimeCSV")
@@ -11,3 +12,7 @@ df2 = df[["REPORTED_DATE", "NEIGHBORHOOD_ID" , "OFFENSE_CATEGORY_ID"]].copy()
 df2.loc[:,"REPORTED_DATE"] = pd.to_datetime(df2.loc[:,"REPORTED_DATE"])
 df2.set_index('REPORTED_DATE', drop=True, append=False, inplace=True, verify_integrity=False)
 df2 = df2.sort_index()
+
+markov = FOM()
+markov.learn(df2, ["NEIGHBORHOOD_ID", "OFFENSE_CATEGORY_ID"])
+pred = markov.choose() # returns dict of predictions for each neighborhood
